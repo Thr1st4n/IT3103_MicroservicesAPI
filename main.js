37e9d6a0c0d75a1663d5832d32aa824068bc8642
Thr1st4n <123819032+Thr1st4n@users.noreply.github.com>
2024-09-19 01:19:12 +0800

    Adding main.js to be used as central script to start all microservices

---
+++ main.js
@@ -0,0 +1,25 @@
+const { exec } = require('child_process');
+
+// Start Product Service
+exec('node product-service/server.js', (err, stdout, stderr) => {
+    if (err) {
+        console.error(`Error starting Product Service: ${err.message}`);
+    }
+    console.log(`Product Service: ${stdout}`);
+});
+
+// Start Customer Service
+exec('node customer-service/server.js', (err, stdout, stderr) => {
+    if (err) {
+        console.error(`Error starting Customer Service: ${err.message}`);
+    }
+    console.log(`Customer Service: ${stdout}`);
+});
+
+// Start Order Service
+exec('node order-service/server.js', (err, stdout, stderr) => {
+    if (err) {
+        console.error(`Error starting Order Service: ${err.message}`);
+    }
+    console.log(`Order Service: ${stdout}`);
+});
